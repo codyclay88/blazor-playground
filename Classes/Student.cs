e00de829bf983c2fcceddcd9627036065a0b3c12
Cody Clay <codyclay88@gmail.com>
2018-09-28 17:23:01 -0400

    First Commit

---
+++ Classes/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Student
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Status { get; private set; }
+
+        public Student(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Status = "waiting";
+        }
+
+        public void SetStatus(string newStatus)
+        {
+            Status = newStatus;
+        }
+    }
+}
